4e3ef73e79f9200f73e5634307b1f0fe5a4aad34
Sudharaka <sudharakafernandobusiness@gmail.com>
2022-12-20 11:57:08 +0530

    Updated DBHelper class and created database

--- app/src/main/java/com/example/toyfirstmobile/Admin.java
+++ app/src/main/java/com/example/toyfirstmobile/Admin.java
@@ -6,10 +6,15 @@ import android.widget.Toast;
 
 public class Admin extends User{
 
-    public Admin(){}
+    DBHelper db;
 
-    public Admin(String userName, String password) {
+    public Admin(){
+        //db = new DBHelper();
+    }
+
+    public Admin(String userName, String password,DBHelper db) {
         super(userName, password);
+        //db = new DBHelper();
     }
 
     //Create toy method, combine with create toy activity in the interface
@@ -20,7 +25,7 @@ public class Admin extends User{
         toy.setName(toyName);
         */
 
-        DBHelper db = new DBHelper();
+
         boolean res=db.insertToyData(toyID, toyName, toyPrice, toyQuantity, toyCategory, toyImage);
         if(res)
 //            Toast.makeText(Admin.this,"Toy created successfully", Toast.LENGTH_SHORT).show();
@@ -32,7 +37,7 @@ public class Admin extends User{
 
     //Update toy method, combine with update toy activity in the interface
     public void updateToy(int toyID, String toyName, float toyPrice, int toyQuantity, int toyCategory, byte[] toyImage){
-        DBHelper db = new DBHelper();
+        //DBHelper db = new DBHelper();
         boolean res=db.updateToyData(toyID, toyName, toyPrice, toyQuantity, toyCategory, toyImage);
         if(res)
 //            Toast.makeText(Admin.this,"Toy updated successfully", Toast.LENGTH_SHORT).show();
@@ -45,7 +50,7 @@ public class Admin extends User{
 
     //Delete toy method, combine with delete toy activity in the interface
     public void deleteToy(int toyID) {
-        DBHelper db = new DBHelper();
+        //DBHelper db = new DBHelper();
         boolean res = db.deleteToyData(toyID);
         if (res)
 //            Toast.makeText(Admin.this,"Toy deleted successfully", Toast.LENGTH_SHORT).show();
@@ -56,7 +61,7 @@ public class Admin extends User{
     }
         //get toy data method, combine with get toy activity in the interface
         public void getToy(int toyID){
-            DBHelper db = new DBHelper();
+            //DBHelper db = new DBHelper();
             Cursor res=db.getToyData();
             if(res.getCount() == 0) {
                 // show message
@@ -78,6 +83,70 @@ public class Admin extends User{
             //put above data into a table
 
         }
+
+        public void createCategory(int categoryID,String categoryName){
+
+
+            boolean res = db.insertToyCategoryData(categoryID,categoryName);
+
+            if(res)
+//            Toast.makeText(Admin.this,"Toy created successfully", Toast.LENGTH_SHORT).show();
+                System.out.println("Category created successfully"); //its better to make toast message
+            else
+//            Toast.makeText("Toy created failed", Toast.LENGTH_SHORT).show();
+                System.out.println("Category creation failed");
+
+
+        }
+
+    //Update category method, combine with update category activity in the interface
+    public void updateCategory(int categoryID,String categoryName){
+        //DBHelper db = new DBHelper();
+        boolean res=db.updateToyCategoryData(categoryID,categoryName);
+        if(res)
+//            Toast.makeText(Admin.this,"Toy updated successfully", Toast.LENGTH_SHORT).show();
+            System.out.println("Category updated successfully");
+        else
+//            Toast.makeText("Toy updated failed", Toast.LENGTH_SHORT).show();
+            System.out.println("Failed updating the category");
+
+    }
+
+    //Delete category method, combine with delete category activity in the interface
+    public void deleteCategory(int toyID) {
+        //DBHelper db = new DBHelper();
+        boolean res = db.deleteToyData(toyID);
+        if (res)
+//            Toast.makeText(Admin.this,"Toy deleted successfully", Toast.LENGTH_SHORT).show();
+            System.out.println("Category deleted successfully");
+        else
+//            Toast.makeText("Toy deleted failed", Toast.LENGTH_SHORT).show();
+            System.out.println("Failed deleting category");
+    }
+    //get toy data method, combine with get toy activity in the interface
+    public void getCategory(int categoryID){
+        //DBHelper db = new DBHelper();
+        Cursor res=db.getToyCategoryData();
+        if(res.getCount() == 0) {
+            // show message
+            System.out.println("Nothing found"); //its better to make toast message
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            buffer.append("Category ID :"+ res.getString(0)+"\n");
+            buffer.append("Category Name :"+ res.getString(1)+"\n");
+
+        }
+        // Show all data
+//            showMessage("Toy Data",buffer.toString());
+        //put above data into a table
+
+    }
+
+
+
 
 
 }
--- app/src/main/java/com/example/toyfirstmobile/MainActivity.java
+++ app/src/main/java/com/example/toyfirstmobile/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.toyfirstmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    Admin admin;
+    DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        admin = new Admin();
+        db = new DBHelper(this);
+        db.insertToyCategoryData(1,"Handmade");
+      //  System.out.print("Hello");
+        Log.d("Creation","Created");
+
+
+
     }
 }
