7b37b0b121b2becab4130da7597e160f9aae2488
rusirugunaratne <info.rusiru@gmail.com>
2022-12-23 20:41:39 +0530

    Input validation added

--- app/src/main/java/com/example/toyfirstmobile/MainActivity.java
+++ app/src/main/java/com/example/toyfirstmobile/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +16,7 @@ import com.example.toyfirstmobile.adapter.CategoryAdapter;
 import com.example.toyfirstmobile.db.DBHelper;
 import com.example.toyfirstmobile.model.Admin;
 import com.example.toyfirstmobile.model.Category;
+import com.example.toyfirstmobile.validation.Validation;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ app/src/main/java/com/example/toyfirstmobile/validation/Validation.java
@@ -0,0 +1,28 @@
+package com.example.toyfirstmobile.validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+
+    public boolean isValidEmail(String email){
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        Pattern p = java.util.regex.Pattern.compile(ePattern);
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+    public boolean isValidUserName(String userName){
+        Pattern pattern = Pattern.compile("[A-Za-z0-9_]+");
+        return (userName != null) && pattern.matcher(userName).matches();
+    }
+
+    public boolean isValidMobileNumber(String mobileNumber){
+        String pattern = "\\+\\d{1,3}-[()\\d\\s-]+";
+        Pattern p = Pattern.compile(pattern);
+        Matcher matcher = p.matcher(mobileNumber);
+        return matcher.matches();
+    }
+
+
+}
