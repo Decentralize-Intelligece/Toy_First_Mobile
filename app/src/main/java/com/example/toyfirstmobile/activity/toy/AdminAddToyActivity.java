48344f5857bd6629f4f2fedda1158a83699a4cc1
rusirugunaratne <info.rusiru@gmail.com>
2022-12-25 00:50:08 +0530

    adding toys interface

--- app/src/main/java/com/example/toyfirstmobile/activity/toy/AdminAddToyActivity.java
+++ app/src/main/java/com/example/toyfirstmobile/activity/toy/AdminAddToyActivity.java
@@ -1,8 +1,15 @@
 package com.example.toyfirstmobile.activity.toy;
 
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
@@ -10,14 +17,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.toyfirstmobile.R;
 import com.example.toyfirstmobile.db.DBHelper;
-import com.example.toyfirstmobile.model.Category;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class AdminAddToyActivity extends AppCompatActivity {
 
     private ArrayList<String> categoryList = new ArrayList<>();
     DBHelper dbHelper = new DBHelper(this);
+    Uri outPutFileUri;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,13 +33,27 @@ public class AdminAddToyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.admin_add_toy);
+        Button btnAddImage = (Button) findViewById(R.id.btnAddToyImage);
+        btnAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
         initData();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, categoryList);
         Spinner spinner = (Spinner) findViewById(R.id.spinnerCategories);
         spinner.setAdapter(adapter);
 
 
+
+
+
     }
+
+
 
     private void initData() {
         categoryList = new ArrayList<>();
