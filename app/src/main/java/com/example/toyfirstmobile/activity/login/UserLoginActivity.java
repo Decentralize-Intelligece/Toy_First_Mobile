944616956660c3135c50bec414a3ce458bf30b74
Pasindu Madhuwantha <73535785+PasinduMaduwantha@users.noreply.github.com>
2022-12-25 11:58:09 +0530

    redirect to relevant dshbrd when admin/user logged in

--- app/src/main/java/com/example/toyfirstmobile/activity/login/UserLoginActivity.java
+++ app/src/main/java/com/example/toyfirstmobile/activity/login/UserLoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.toyfirstmobile.activity.login;
 import com.example.toyfirstmobile.R;
+import com.example.toyfirstmobile.activity.dashboards.AdminDashboardActivity;
 import com.example.toyfirstmobile.activity.dashboards.UserDashboardActivity;
 import com.example.toyfirstmobile.db.DBHelper;
 
@@ -25,56 +26,46 @@ public class UserLoginActivity extends AppCompatActivity {
             /**take email and password from user, check if email and password are correct
              * if correct, go to main page, if not, show error message
              */
-
-
             btnLogin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     EditText txtEmail = (EditText) findViewById(R.id.editTextEmail);
-                    String email=txtEmail.getText().toString();
+                    String email = txtEmail.getText().toString();
                     //compare password is correct
                     EditText txtPassword = (EditText) findViewById(R.id.editTextPassword);
-                    String password=txtPassword.getText().toString();
-                    Log.d("password",password);
-//                    dbHelper.getUserDataByEmail(email);
-                    Log.d("email",email);
-                    Cursor res=null;
+                    String password = txtPassword.getText().toString();
+                    Log.d("email", email);
+                    Log.d("password", password);
+
+                    Cursor res = null;
                     try {
-                        res= dbHelper.getUserDataByEmail(email);
-
+                        res = dbHelper.getUserDataByEmail(email);
+                    } catch (Exception e) {
+                        Log.d("error", e.getMessage());
                     }
-                    catch (Exception e){
-                        Log.d("error",e.getMessage());
-                    }
-                    if(res.getCount()==0){
-                        Log.d("UserLoginActivity","No data");
-                    }
-                    else{
-                        while(res.moveToNext()){
-                            Log.d("UserLoginActivity","email: "+res.getString(1));
-                            Log.d("UserLoginActivity","username: "+res.getString(2));
-                            Log.d("UserLoginActivity","password: "+res.getString(3));
-                            Log.d("UserLoginActivity","isadmin: "+res.getString(4));
-                            Log.d("UserLoginActivity","name: "+res.getString(5));
-                            Log.d("UserLoginActivity","address: "+res.getString(6));
-                            Log.d("UserLoginActivity","mobile: "+res.getString(7));
-                        }
-
-                        if(res.moveToFirst()) {
+                    if (res.getCount() == 0) {
+                        Log.d("UserLoginActivity", "No data");
+                    } else {
+                        if (res.moveToFirst()) {
                             if (res.getString(3).equals(password)) {
                                 Log.d("UserLoginActivity", "password correct");
                                 //go to the user home page
-                                Intent intent = new Intent(v.getContext(), UserDashboardActivity.class);
-                                startActivity(intent);
-
-                            } else {
-                                Log.d("UserLoginActivity", "password incorrect");
+                                if (res.getInt(4) == 0) {
+                                    Log.d("UserLoginActivity", "user");
+                                    Intent intent = new Intent(v.getContext(), UserDashboardActivity.class);
+                                    v.getContext().startActivity(intent);
+                                } else {
+                                    Log.d("UserLoginActivity", "admin");
+                                    Intent intent = new Intent(v.getContext(), AdminDashboardActivity.class);
+                                    v.getContext().startActivity(intent);
+                                }
                             }
+                        } else {
+                            Log.d("UserLoginActivity", "password incorrect");
                         }
                     }
                     res.close();
                     dbHelper.close();
-
                 }
             });
         }
