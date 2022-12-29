73ce158fad2503d8272238734c7f678bb1368880
Sudharaka <sudharakafernandobusiness@gmail.com>
2022-12-29 20:34:56 +0530

    Validating the user registration is finished

--- app/src/main/java/com/example/toyfirstmobile/validation/Validation.java
+++ app/src/main/java/com/example/toyfirstmobile/validation/Validation.java
@@ -5,20 +5,20 @@ import java.util.regex.Pattern;
 
 public class Validation {
 
-    public boolean isValidEmail(String email){
+    public static boolean isValidEmail(String email){
         String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         Pattern p = java.util.regex.Pattern.compile(ePattern);
         Matcher m = p.matcher(email);
         return m.matches();
     }
 
-    public boolean isValidUserName(String userName){
+    public static boolean isValidUserName(String userName){
         Pattern pattern = Pattern.compile("[A-Za-z0-9_]+");
         return (userName != null) && pattern.matcher(userName).matches();
     }
 
-    public boolean isValidMobileNumber(String mobileNumber){
-        String pattern = "\\+\\d{1,3}-[()\\d\\s-]+";
+    public static boolean isValidMobileNumber(String mobileNumber){
+        String pattern = "(0|94)?[0-9][0-9]{9}";
         Pattern p = Pattern.compile(pattern);
         Matcher matcher = p.matcher(mobileNumber);
         return matcher.matches();
